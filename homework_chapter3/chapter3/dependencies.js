0c48ea76306af67e64ee0f51ed01638c89b212fd
Leonardo Rodriguez <lrodriguez@localhost.localdomain>
2017-03-20 20:29:10 -0500

    Se agrega tarea del capitulo 4 y se borrar las carpetas node_modules

---
+++ homework_chapter3/chapter3/dependencies.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+var fx = require('./fx');
+var Stripe = require('stripe');
+
+module.exports = function(wagner) {
+
+  wagner.factory('Config', function() {
+    return JSON.parse(fs.readFileSync('./config.json').toString());
+  });
+
+
+  var stripe =
+
+  // TODO: Make Stripe depend on the Config service and use its `stripeKey`
+  // property to get the Stripe API key.
+  wagner.factory('Stripe', function(Config) {
+    return Stripe(Config.stripeKey);
+  });
+
+  wagner.factory('fx', fx);
+
+
+};
